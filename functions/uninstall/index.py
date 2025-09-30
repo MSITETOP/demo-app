bc98e6dc77a117ae8ec34049fd9e81fee106803e
ka@ipgpromo.ru <ka@ipgpromo.ru>
2025-09-30 12:07:22 +0200

    add uninstall

---
+++ functions/uninstall/index.py
@@ -0,0 +1,22 @@
+import os
+import json
+import base64
+import requests
+from urllib.parse import parse_qs
+from libs.applogs import logger
+from libs.db import installApp, activitySet
+
+async def handler(event, context):
+    postQuery = base64.b64decode(event.get("body").encode('ascii')).decode('ascii')
+    arPost = dict((k, v[-1] if isinstance(v, list) else v)
+      for k, v in parse_qs(postQuery).items())
+       
+    logger.debug(arPost)
+        
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'text/html',
+        },  
+        'body': 'ok',
+    }
